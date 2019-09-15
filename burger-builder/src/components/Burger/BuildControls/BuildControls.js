76f72156028cd3d7c5e900ade62692170d0bb236
Arthur Lee <ruhtraeel@gmail.com>
2019-09-14 21:02:14 -0700

    Chapter 8 Lesson 18: Adding the Order Button

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import classes from './BuildControls.css';
 import BuildControl from './BuildControl/BuildControl';
+import { isClassExpression } from '@babel/types';
 
 const controls = [
     { label: 'Salad', type: 'salad'},
@@ -21,6 +22,10 @@ const buildControls = (props) => (
                 removed={() => props.ingredientRemoved(ctrl.type)}
                 disabled={props.disabled[ctrl.type]}/>
         ))}
+        <button
+            className={classes.OrderButton}
+            disabled={!props.purchasable}
+        >ORDER NOW</button>
     </div>
 );
 
