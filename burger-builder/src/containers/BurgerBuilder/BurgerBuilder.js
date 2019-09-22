7402e53b313141925fede11a4edf2c0c5ecaaaf4
Arthur Lee <ruhtraeel@gmail.com>
2019-09-22 14:05:46 -0700

    Chapter 12 Lesson 8: Order Submission & Passing Data between Pages

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -94,36 +94,14 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        ////alert('You continue!');
-        //this.setState({ loading: true });
-        //// Calculate price on server to prevent users from changing it in their browser
-        //const order = {
-        //    ingredients: this.state.ingredients,
-        //    price: this.state.totalPrice,
-        //    customer: {
-        //        name: 'Arthur Lee',
-        //        address: {
-        //            street: 'Teststreet 1',
-        //            zipCode: '12345',
-        //            country: 'Canada'
-        //        },
-        //        email: 'test@test.com'
-        //    },
-        //    deliveryMethod: 'fastest'
-        //}
-        //axios.post('/orders.json', order)
-        //    .then(response => {
-        //        this.setState({ loading: false, purchasing: false });
-        //    })
-        //    .catch(error => {
-        //        this.setState({ loading: false, purchasing: false });
-        //    });
+
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) +
             '=' +
             encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
 
         const queryString = queryParams.join('&');
         this.props.history.push({
