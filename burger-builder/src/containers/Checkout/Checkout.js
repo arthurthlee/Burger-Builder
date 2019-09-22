25afb34de3d1c3e477e2238901bb3c95a0d3f311
Arthur Lee <ruhtraeel@gmail.com>
2019-09-22 01:29:51 -0700

    Chapter 12 Lesson 7: Navigating to the Contact Data Component

--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
+
+import { Route } from 'react-router-dom';
 
 class Checkout extends Component {
     state = {
@@ -35,8 +38,8 @@ class Checkout extends Component {
                 <CheckoutSummary
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler}>
-                </CheckoutSummary>
+                    checkoutContinued={this.checkoutContinuedHandler} />
+                <Route path={this.props.match.path + '/contact-data'} component={ContactData}/>
             </div>
         );
     }
