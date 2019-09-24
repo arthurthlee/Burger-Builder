ebc99036b3082cdcd563f43651011e1866a609e4
Arthur Lee <ruhtraeel@gmail.com>
2019-09-23 21:59:05 -0700

    Chapter 13 Lesson 5: Dynamically Create Inputs based on JS Config

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -53,57 +53,67 @@ class ContactData extends Component {
                 elementType: 'select',
                 elementConfig: {
                     options: [
-                        {value: 'fastest', displayValue: 'Fastest'},
-                        {value: 'cheapest', displayValue: 'Cheapest'},
+                        { value: 'fastest', displayValue: 'Fastest' },
+                        { value: 'cheapest', displayValue: 'Cheapest' },
                     ]
                 },
                 value: ''
             },
-            loading: false
         }
     }
 
     orderHandler = (event) => {
-            event.preventDefault();
-            //alert('You continue!');
-            this.setState({ loading: true });
-            // Calculate price on server to prevent users from changing it in their browser
-            const order = {
-                ingredients: this.props.ingredients,
-                price: this.props.price,
+        event.preventDefault();
+        //alert('You continue!');
+        this.setState({ loading: true });
+        // Calculate price on server to prevent users from changing it in their browser
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
 
-            }
-            axios.post('/orders.json', order)
-                .then(response => {
-                    this.setState({ loading: false });
-                    this.props.history.push('/');
-                })
-                .catch(error => {
-                    this.setState({ loading: false });
-                });
-            console.log(this.props.ingredients);
         }
-
-    render() {
-            let form = (
-                <form>
-                    <Input elementType="..." elementConfig="..." value="..."/>
-                    <Input inputType="input" type="email" name="email" placeholder="Your Mail" />
-                    <Input inputType="input" type="text" name="street" placeholder="Street" />
-                    <Input inputType="input" type="text" name="postal" placeholder="Postal Code" />
-                    <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-                </form>
-            );
-            if (this.state.loading) {
-                form = <Spinner />;
-            }
-            return (
-                <div className={classes.ContactData}>
-                    <h4>Enter your Contact Data</h4>
-                    {form}
-                </div>
-            );
-        }
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({ loading: false });
+                this.props.history.push('/');
+            })
+            .catch(error => {
+                this.setState({ loading: false });
+            });
+        console.log(this.props.ingredients);
     }
 
-    export default ContactData;
+    render() {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+        let form = (
+            <form>
+                {formElementsArray.map(formElement => (
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                    />
+                ))}
+                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            </form>
+        );
+        if (this.state.loading) {
+            form = <Spinner />;
+        }
+        return (
+            <div className={classes.ContactData}>
+                <h4>Enter your Contact Data</h4>
+                {form}
+            </div>
+        );
+    }
+}
+
+export default ContactData;
