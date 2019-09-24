bb03ba9c8760a6c47d278e09848532231d246ed1
Arthur Lee <ruhtraeel@gmail.com>
2019-09-23 21:51:19 -0700

    Chapter 13 Lesson 4: Setting Up a JS Config for the Form

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -7,65 +7,102 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: '',
-        },
-        loading: false
-    }
+        orderForm: {
+
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP CODE'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Email'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'},
+                    ]
+                },
+                value: ''
+            },
+            loading: false
+        }
 
     orderHandler = (event) => {
-        event.preventDefault();
-        //alert('You continue!');
-        this.setState({ loading: true });
-        // Calculate price on server to prevent users from changing it in their browser
-        const order = {
-            ingredients: this.props.ingredients,
-            price: this.props.price,
-            customer: {
-                name: 'Arthur Lee',
-                address: {
-                    street: 'Teststreet 1',
-                    zipCode: '12345',
-                    country: 'Canada'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+            event.preventDefault();
+            //alert('You continue!');
+            this.setState({ loading: true });
+            // Calculate price on server to prevent users from changing it in their browser
+            const order = {
+                ingredients: this.props.ingredients,
+                price: this.props.price,
+
+            }
+            axios.post('/orders.json', order)
+                .then(response => {
+                    this.setState({ loading: false });
+                    this.props.history.push('/');
+                })
+                .catch(error => {
+                    this.setState({ loading: false });
+                });
+            console.log(this.props.ingredients);
         }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false});
-                this.props.history.push('/');
-            })
-            .catch(error => {
-                this.setState({ loading: false});
-            });
-        console.log(this.props.ingredients);
+
+    render() {
+            let form = (
+                <form>
+                    <Input inputType="input" type="text" name="name" placeholder="Your name" />
+                    <Input inputType="input" type="email" name="email" placeholder="Your Mail" />
+                    <Input inputType="input" type="text" name="street" placeholder="Street" />
+                    <Input inputType="input" type="text" name="postal" placeholder="Postal Code" />
+                    <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+                </form>
+            );
+            if (this.state.loading) {
+                form = <Spinner />;
+            }
+            return (
+                <div className={classes.ContactData}>
+                    <h4>Enter your Contact Data</h4>
+                    {form}
+                </div>
+            );
+        }
     }
 
-    render () {
-        let form = (
-            <form>
-                <Input inputType="input" type="text" name="name" placeholder = "Your name" />
-                <Input inputType="input" type="email" name="email" placeholder = "Your Mail" />
-                <Input inputType="input" type="text" name="street" placeholder = "Street" />
-                <Input inputType="input" type="text" name="postal" placeholder = "Postal Code" />
-                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-            </form>
-        );
-        if (this.state.loading) {
-            form = <Spinner />;
-        }
-        return (
-            <div className={classes.ContactData}>
-                <h4>Enter your Contact Data</h4>
-                {form}
-            </div>
-        );
-    }
-}
-
-export default ContactData;
+    export default ContactData;
