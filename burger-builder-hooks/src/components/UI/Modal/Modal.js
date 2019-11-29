4762479ee5a2656e6d95aa113d1009f6d8b48bcb
Arthur Lee <ruhtraeel@gmail.com>
2019-11-28 16:06:42 -0800

    Chapter 27 Lesson 10: Using React.memo() & More

--- burger-builder-hooks/src/components/UI/Modal/Modal.js
+++ burger-builder-hooks/src/components/UI/Modal/Modal.js
@@ -1,31 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop'
 import Aux from '../../../hoc/Aux/Aux';
 
-class Modal extends Component{
+const Modal = props => {
 
-    shouldComponentUpdate(nextProps, nextState) {
+    /*shouldComponentUpdate(nextProps, nextState) {
         return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
-    }
+    }*/
 
-    render() {
-        return (
-            <Aux>
-                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
-                <div
-                    className={classes.Modal}
-                    style={{
-                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                        opacity: this.props.show ? '1' : '0'
-                    }}>
-                    {this.props.children}
-                </div>
-            </Aux>
-        );
-    }
+    return (
+        <Aux>
+            <Backdrop show={props.show} clicked={props.modalClosed} />
+            <div
+                className={classes.Modal}
+                style={{
+                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0'
+                }}>
+                {props.children}
+            </div>
+        </Aux>
+    );
 }
 
-
-export default Modal;
+// Only update this component when its props are updated and nextprops.show !== prevprops.show, same with children
+export default React.memo(Modal, (prevProps, nextProps) =>
+    nextProps.show === prevProps.show &&
+    nextProps.children === prevProps.children);
